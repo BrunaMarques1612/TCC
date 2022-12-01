c9a50e91e2678f7fa912c53b99b2b55c0dc94c0c
youexample <you@example.com>
2022-11-30 22:22:28 -0300

    eu sou nice

--- web/livros.php
+++ web/livros.php
@@ -15,7 +15,141 @@ include 'includes/nav-bar.php';
   //PRELOARD
   include 'includes/preloard.php';
   ?>
+  
+  <?php
+  //Livros
+  ?>
 
+<head>  
+
+<link rel="stylesheet" href="./style.css" />
+
+</head>
+<body>
+  
+ <h1 class = "Titulo_Acervo">Acervo da Biblioteca</h1>
+
+<section class ="Acervo_Livros">
+
+
+  
+
+
+
+
+  <div class="box_Acervo">
+    <div class="BBcontainer">
+        <div class="livros__lista">
+            <div class="loader" id="loaderGif"></div>
+                <div class="livros__item acervo-item">
+
+                    <div class="trig" style="display: none"></div>
+
+                    <a href="img/livros/Livro01.jpg" class="livros__imagem">
+                        <img data-src="https://staticbv.am4.com.br/publicacoes/2/thumbs/thumbnail_397_x_595.jpg" class=" ls-is-cached lazyloaded" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/2/thumbs/thumbnail_397_x_595.jpg">
+                    </a>
+                    <div class="book__stars">
+                        <i class="fas fa-star active"></i>
+                        <i class="fas fa-star active"></i>
+                        <i class="fas fa-star active"></i>
+                        <i class="fas fa-star "></i>
+                        <i class="fas fa-star "></i>
+                        <i class="votos">(19)</i>
+                    </div>
+                    <a href="/NossoAcervo/Publicacao/2" class="livros__title">Construindo Redes Cisco Escaláveis</a>
+                    <a href="/NossoAcervo/Publicacao/2" class="livros__author">Paquet, Catherine; Teare, Diane</a>
+                </div>
+
+
+
+
+                <div class="T">
+    <div class = "Box_Livros">
+      <div class="Container_Livros">
+        <img src="img/livros/Livro01.jpg" >
+        <a href="">O Pequeno Principe</a>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="T">
+<div class = "Box_Livros">
+<div class="Container_Livros">
+  <img src="img/livros/Livro02.jpg" >
+  
+  <a href="">O Menino Maluquinho</a>
+</div>
+</div>
+</div>
+
+<div class="T">
+<div class = "Box_Livros">
+<div class="Container_Livros">
+  <img src="img/livros/Livro03.jpg" >
+  
+  <a href="">Diario de um Banana</a>
+</div>
+</div>
+</div>
+
+<div class="T">
+<div class = "Box_Livros">
+<div class="Container_Livros">
+  <img src="img/livros/Livro04.jpg" >
+  <a href="">Rita, Não Grita</a>
+</div>
+</div>
+</div>
+
+<div class="T">
+<div class = "Box_Livros">
+<div class="Container_Livros">
+  <img src="img/livros/Livro05.jpg" >
+  
+  <a href="">Chapeuzinho Vermelho</a>
+</div>
+</div>
+</div>
+
+<div class="T">
+<div class = "Box_Livros">
+<div class="Container_Livros">
+  <img src="img/livros/Livro02.jpg" >
+  
+  <a href="">O Pequeno Principe</a>
+</div>
+</div>
+</div>
+
+<div class="T">
+<div class = "Box_Livros">
+<div class="Container_Livros">
+  <img src="img/livros/Livro01.jpg" >
+  
+  <a href="">O Pequeno Principe</a>
+</div>
+</div>
+</div>
+
+</section>
+
+</body>
+</html>
   <?php
   //FOOTER
   include 'includes/footer.php';
