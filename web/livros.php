1b21c600fa7aaf80849d1e4ccb92d7808b271970
MatheusSantos170 <99297398+MatheusSantos170@users.noreply.github.com>
2022-12-01 22:17:44 -0300

    Add files via upload

--- web/livros.php
+++ web/livros.php
@@ -37,15 +37,15 @@ include 'includes/nav-bar.php';
 
 
 
-  <div class="box_Acervo">
-    <div class="BBcontainer">
+<div class="box_nossoAcervo">
+    <div class="ui-container">
         <div class="livros__lista">
             <div class="loader" id="loaderGif"></div>
-                <div class="livros__item acervo-item">
+                <div class="livros__item acervo-item epub">
 
                     <div class="trig" style="display: none"></div>
 
-                    <a href="img/livros/Livro01.jpg" class="livros__imagem">
+                    <a href="/NossoAcervo/Publicacao/2" class="livros__imagem">
                         <img data-src="https://staticbv.am4.com.br/publicacoes/2/thumbs/thumbnail_397_x_595.jpg" class=" ls-is-cached lazyloaded" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/2/thumbs/thumbnail_397_x_595.jpg">
                     </a>
                     <div class="book__stars">
@@ -56,10 +56,9 @@ include 'includes/nav-bar.php';
                         <i class="fas fa-star "></i>
                         <i class="votos">(19)</i>
                     </div>
-                    <a href="/NossoAcervo/Publicacao/2" class="livros__title">Construindo Redes Cisco Escaláveis</a>
+                    <a href="/NossoAcervo/Publicacao/2" class="livros__title">O Pequeno Principe</a>
                     <a href="/NossoAcervo/Publicacao/2" class="livros__author">Paquet, Catherine; Teare, Diane</a>
                 </div>
-
 
 
 
@@ -79,71 +78,58 @@ include 'includes/nav-bar.php';
 
 
 
-
-
-
-
-
-
-
-
 <div class="T">
-<div class = "Box_Livros">
-<div class="Container_Livros">
-  <img src="img/livros/Livro02.jpg" >
-  
-  <a href="">O Menino Maluquinho</a>
-</div>
-</div>
+  <div class = "Box_Livros">
+    <div class="Container_Livros">
+      <img src="img/livros/Livro02.jpg" >
+      <a href="">O Menino Maluquinho</a>
+    </div>
+  </div>
 </div>
 
 <div class="T">
-<div class = "Box_Livros">
-<div class="Container_Livros">
-  <img src="img/livros/Livro03.jpg" >
-  
-  <a href="">Diario de um Banana</a>
-</div>
-</div>
-</div>
-
-<div class="T">
-<div class = "Box_Livros">
-<div class="Container_Livros">
-  <img src="img/livros/Livro04.jpg" >
-  <a href="">Rita, Não Grita</a>
-</div>
-</div>
+  <div class = "Box_Livros">
+    <div class="Container_Livros">
+      <img src="img/livros/Livro03.jpg" >
+      <a href="">Diario de um Banana</a>
+    </div>
+  </div>
 </div>
 
 <div class="T">
-<div class = "Box_Livros">
-<div class="Container_Livros">
-  <img src="img/livros/Livro05.jpg" >
-  
-  <a href="">Chapeuzinho Vermelho</a>
-</div>
-</div>
-</div>
-
-<div class="T">
-<div class = "Box_Livros">
-<div class="Container_Livros">
-  <img src="img/livros/Livro02.jpg" >
-  
-  <a href="">O Pequeno Principe</a>
-</div>
-</div>
+  <div class = "Box_Livros">
+    <div class="Container_Livros">
+      <img src="img/livros/Livro04.jpg" >
+      <a href="">Rita, Não Grita</a>
+    </div>
+  </div>
 </div>
 
 <div class="T">
-<div class = "Box_Livros">
-<div class="Container_Livros">
-  <img src="img/livros/Livro01.jpg" >
-  
-  <a href="">O Pequeno Principe</a>
+  <div class = "Box_Livros">
+    <div class="Container_Livros">
+      <img src="img/livros/Livro05.jpg" >
+      <a href="">Chapeuzinho Vermelho</a>
+    </div>
+  </div>
 </div>
+
+<div class="T">
+  <div class = "Box_Livros">
+    <div class="Container_Livros">
+      <img src="img/livros/Livro02.jpg" >
+      <a href="">O Pequeno Principe</a>
+    </div>
+  </div>
 </div>
+
+<div class="T">
+  <div class = "Box_Livros">
+    <div class="Container_Livros">
+      <img src="img/livros/Livro01.jpg" >
+      <a href="">O Pequeno Principe</a>
+    </div>
+  </div>
 </div>
 
 </section>
