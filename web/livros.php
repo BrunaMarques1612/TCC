9ce40add40ab1992946fcb01583b94cbad5c45de
MarcosSouza7 <102393589+MarcosSouza7@users.noreply.github.com>
2022-12-03 02:17:28 -0300

    commit pronto

--- web/livros.php
+++ web/livros.php
@@ -19,123 +19,128 @@ include 'includes/nav-bar.php';
   <?php
   //Livros
   ?>
+<html>
+    <head>
+        <link rel="stylesheet" href="./style.css" />
+    </head>
+    <body>
+        <h1>Acervo Lima de Matos</h1>
 
-<head>  
-
-<link rel="stylesheet" href="./style.css" />
-
-</head>
-<body>
-  
- <h1 class = "Titulo_Acervo">Acervo da Biblioteca</h1>
-
-<section class ="Acervo_Livros">
-
-
-  
-
-
-
-
-<div class="box_nossoAcervo">
+    <div class="box_nossoAcervo">
     <div class="ui-container">
         <div class="livros__lista">
             <div class="loader" id="loaderGif"></div>
-                <div class="livros__item acervo-item epub">
+                <div class="livros__item acervo-item ">
 
                     <div class="trig" style="display: none"></div>
 
-                    <a href="/NossoAcervo/Publicacao/2" class="livros__imagem">
-                        <img data-src="https://staticbv.am4.com.br/publicacoes/2/thumbs/thumbnail_397_x_595.jpg" class=" ls-is-cached lazyloaded" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/2/thumbs/thumbnail_397_x_595.jpg">
+                    <a href="web\img\livros\harry.jpg" class="livros__imagem">
+                        <img class=" lazyloaded"src = 'web\img\livros\harry.jpg' width="120" height="180" >
                     </a>
-                    <div class="book__stars">
-                        <i class="fas fa-star active"></i>
-                        <i class="fas fa-star active"></i>
-                        <i class="fas fa-star active"></i>
-                        <i class="fas fa-star "></i>
-                        <i class="fas fa-star "></i>
-                        <i class="votos">(19)</i>
-                    </div>
-                    <a href="/NossoAcervo/Publicacao/2" class="livros__title">O Pequeno Principe</a>
+                    
+                    <a href="/NossoAcervo/Publicacao/2" class="livros__title">Construindo Redes Cisco Escaláveis</a>
                     <a href="/NossoAcervo/Publicacao/2" class="livros__author">Paquet, Catherine; Teare, Diane</a>
+                </div>
+                
+
+                <div class="livros__item acervo-item ">
+
+                    <div class="trig" style="display: none"></div>
+
+                    <a href="/NossoAcervo/Publicacao/15" class="livros__imagem">
+                        <img data-src="https://staticbv.am4.com.br/publicacoes/15/thumbs/thumbnail_397_x_595.jpg" class=" lazyloaded" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/15/thumbs/thumbnail_397_x_595.jpg">
+                    </a>
+                    
+                    <a href="/NossoAcervo/Publicacao/15" class="livros__title">Princípios de Economia</a>
+                    <a href="/NossoAcervo/Publicacao/15" class="livros__author">Mochón Morcillo, Francisco</a>
                 </div>
 
 
+                <div class="livros__item acervo-item ">
 
-                <div class="T">
-    <div class = "Box_Livros">
-      <div class="Container_Livros">
-        <img src="img/livros/Livro01.jpg" >
-        <a href="">O Pequeno Principe</a>
-      </div>
+                    <div class="trig" style="display: none"></div>
+
+                    <a href="/NossoAcervo/Publicacao/16" class="livros__imagem">
+                        <img data-src="https://staticbv.am4.com.br/publicacoes/16/thumbs/thumbnail_397_x_595.jpg" class=" lazyloading" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/16/thumbs/thumbnail_397_x_595.jpg">
+                    </a>
+                    
+                    <a href="/NossoAcervo/Publicacao/16" class="livros__title">Jogos de Empresa - 2ª edição</a>
+                    <a href="/NossoAcervo/Publicacao/16" class="livros__author">Gramigna, Maria Rita Miranda</a>
+                </div>
+
+
+                <div class="livros__item acervo-item ">
+
+                    <div class="trig" style="display: none"></div>
+
+                    <a href="/NossoAcervo/Publicacao/17" class="livros__imagem">
+                        <img data-src="https://staticbv.am4.com.br/publicacoes/17/thumbs/thumbnail_397_x_595.jpg" class=" lazyloading" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/17/thumbs/thumbnail_397_x_595.jpg">
+                    </a>
+                    
+                    <a href="/NossoAcervo/Publicacao/17" class="livros__title">Fórmulas e Funções com Microsoft Office Excel 2007</a>
+                    <a href="/NossoAcervo/Publicacao/17" class="livros__author">Mcfedries, Paul</a>
+                </div>
+
+
+                <div class="livros__item acervo-item ">
+
+                    <div class="trig" style="display: none"></div>
+
+                    <a href="/NossoAcervo/Publicacao/19" class="livros__imagem">
+                        <img data-src="https://staticbv.am4.com.br/publicacoes/19/thumbs/thumbnail_397_x_595.jpg" class=" lazyloaded" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/19/thumbs/thumbnail_397_x_595.jpg">
+                    </a>
+                    
+                    <a href="/NossoAcervo/Publicacao/19" class="livros__title">Marketing Global - 7ª edição</a>
+                    <a href="/NossoAcervo/Publicacao/19" class="livros__author">Keegan, Warren Joseph</a>
+                </div>
+
+
+                <div class="livros__item acervo-item ">
+
+                    <div class="trig" style="display: none"></div>
+
+                    <a href="/NossoAcervo/Publicacao/20" class="livros__imagem">
+                        <img data-src="https://staticbv.am4.com.br/publicacoes/20/thumbs/thumbnail_397_x_595.jpg" class=" lazyloading" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/20/thumbs/thumbnail_397_x_595.jpg">
+                    </a>
+                   
+                    <a href="/NossoAcervo/Publicacao/20" class="livros__title">Estatística para Ciências Humanas - 9ª edição</a>
+                    <a href="/NossoAcervo/Publicacao/20" class="livros__author">Levin, Jack; Fox, James Alan</a>
+                </div>
+
+
+                <div class="livros__item acervo-item ">
+
+                    <div class="trig" style="display: none"></div>
+
+                    <a href="/NossoAcervo/Publicacao/21" class="livros__imagem">
+                        <img data-src="https://staticbv.am4.com.br/publicacoes/21/thumbs/thumbnail_397_x_595.jpg" class=" lazyloaded" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/21/thumbs/thumbnail_397_x_595.jpg">
+                    </a>
+                   
+                    <a href="/NossoAcervo/Publicacao/21" class="livros__title">Pré-Cálculo</a>
+                    <a href="/NossoAcervo/Publicacao/21" class="livros__author">Demana, Franklin D. ... [et al.]</a>
+                </div>
+
+
+                <div class="livros__item acervo-item ">
+
+                    <div class="trig" style="display: none"></div>
+
+                    <a href="/NossoAcervo/Publicacao/23" class="livros__imagem">
+                        <img data-src="https://staticbv.am4.com.br/publicacoes/23/thumbs/thumbnail_397_x_595.jpg" class=" lazyloaded" onerror="this.src = '/images/book_default.jpg'" width="120" height="180" src="https://staticbv.am4.com.br/publicacoes/23/thumbs/thumbnail_397_x_595.jpg">
+                    </a>
+                   
+                    <a href="/NossoAcervo/Publicacao/23" class="livros__title">Introdução à Pesquisa de Marketing</a>
+                    <a href="/NossoAcervo/Publicacao/23" class="livros__author">Malhotra, Naresh K. ... [et al.]</a>
+                </div>
+
+        </div>
+
     </div>
-  </div>
 
-
-
-
-
-
-
-
-<div class="T">
-  <div class = "Box_Livros">
-    <div class="Container_Livros">
-      <img src="img/livros/Livro02.jpg" >
-      <a href="">O Menino Maluquinho</a>
-    </div>
-  </div>
 </div>
-
-<div class="T">
-  <div class = "Box_Livros">
-    <div class="Container_Livros">
-      <img src="img/livros/Livro03.jpg" >
-      <a href="">Diario de um Banana</a>
-    </div>
-  </div>
-</div>
-
-<div class="T">
-  <div class = "Box_Livros">
-    <div class="Container_Livros">
-      <img src="img/livros/Livro04.jpg" >
-      <a href="">Rita, Não Grita</a>
-    </div>
-  </div>
-</div>
-
-<div class="T">
-  <div class = "Box_Livros">
-    <div class="Container_Livros">
-      <img src="img/livros/Livro05.jpg" >
-      <a href="">Chapeuzinho Vermelho</a>
-    </div>
-  </div>
-</div>
-
-<div class="T">
-  <div class = "Box_Livros">
-    <div class="Container_Livros">
-      <img src="img/livros/Livro02.jpg" >
-      <a href="">O Pequeno Principe</a>
-    </div>
-  </div>
-</div>
-
-<div class="T">
-  <div class = "Box_Livros">
-    <div class="Container_Livros">
-      <img src="img/livros/Livro01.jpg" >
-      <a href="">O Pequeno Principe</a>
-    </div>
-  </div>
-</div>
-
-</section>
-
 </body>
 </html>
+
   <?php
   //FOOTER
   include 'includes/footer.php';
